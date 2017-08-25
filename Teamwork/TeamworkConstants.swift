f3bd9f6fe4246ff171d61a75cc5e7bbe82d26f2f
David Spector <spector@zeitgeist.com>
2017-08-25 16:51:36 -0700

    prep for partial sync work

--- Teamwork/TeamworkConstants.swift
+++ Teamwork/TeamworkConstants.swift
@@ -56,8 +56,8 @@ struct TeamWorkConstants {
     static let appID = Bundle.main.bundleIdentifier!
     
     // @TODO: Needs to point to either a local host or give directions to user on how to set up RMP Pro Edition
-    static let syncHost                 = "138.197.205.99"            //"192.241.218.72"        //"104.131.145.124"  //"127.0.0.1"// "192.241.218.72" //"127.0.0.1"
-    static let ApplicationName          = "TeamworkMR"
+    static let syncHost                 = "162.243.137.65" // regular teamwork-MR demo server: "138.197.205.99"
+    static let ApplicationName          = "TeamworkPS"
     static let syncRealmPath            = "teamwork"
     static let kSelectedTeamPrefsKey    = "TeamworkSelectedTeam"
     
